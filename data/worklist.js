379f3ad6d06102756c5d71dcaa7f12c2a926ba21
hm0524 <38096718+hm0524@users.noreply.github.com>
2018-10-04 15:43:35 +0900

    Add files via upload

---
+++ data/worklist.js
@@ -0,0 +1,63 @@
+const worklist = [
+	{
+		"id": 1,
+		"title": "いざゆけ!難田学園中等部【第56難】",
+		"subTitle": "【第56難】奴らに気をつけろ",
+		"url": "nanda/nanda56.html"
+	},
+	{
+		"id": 2,
+		"title": "いざゆけ!難田学園中等部【第55難】",
+		"subTitle": "【第55難】臨海学校",
+		"url": "nanda/nanda55.html"
+	},
+	{
+		"id": 3,
+		"title": "いざゆけ!難田学園中等部【第54難】",
+		"subTitle": "【第54難】大輪の華",
+		"url": "nanda/nanda54.html"
+	},
+	{
+		"id": 4,
+		"title": "いざゆけ!難田学園中等部【第53難】",
+		"subTitle": "【第53難】うらめしや",
+		"url": "nanda/nanda53.html"
+	},
+	{
+		"id": 5,
+		"title": "いざゆけ!難田学園中等部【第52難】",
+		"subTitle": "【第52難】金魚すくい",
+		"url": "nanda/nanda52.html"
+	},
+	{
+		"id": 6,
+		"title": "いざゆけ!難田学園中等部【第51難】",
+		"subTitle": "【第51難】難田祭",
+		"url": "nanda/nanda51.html"
+	},
+	{
+		"id": 7,
+		"title": "いざゆけ!難田学園中等部【第50難】",
+		"subTitle": "【第50難】熱き記憶",
+		"url": "nanda/nanda50.html"
+	},
+	{
+		"id": 8,
+		"title": "いざゆけ!難田学園中等部【第49難】",
+		"subTitle": "【第49難】騎馬戦",
+		"url": "nanda/nanda49.html"
+	},
+	{
+		"id": 9,
+		"title": "いざゆけ!難田学園中等部【第48難】",
+		"subTitle": "【第48難】古(いにしえ)の誇り",
+		"url": "nanda/nanda48.html"
+	},
+	{
+		"id": 10,
+		"title": "いざゆけ!難田学園中等部【第47難】",
+		"subTitle": "【第47難】部活対抗リレー",
+		"url": "nanda/nanda47.html"
+	}
+]
+
