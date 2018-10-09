4fe7527e79cbcd6f1dbdf189892b4de4bb5bdb7e
hm0524 <38096718+hm0524@users.noreply.github.com>
2018-10-09 10:04:05 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,27 +4,35 @@
 	// メニュー配置
 	$('#id_menu').load('temp_menu.html', function() {
 
-$.when(
-		// 【設定ファイル】読み込み
-		loadSetting()
-).done(function(){
-		// トップ配置
-		$('#id_main').load('temp_top.html', function() {
-
-			// Viewインスタンス生成 【PICK UP配置】
-			const viewInstancePickup = new viewPickup({
-				// Collectionを渡す
-				collection:collectionInstanceSetting
-			});
+		$.when(
+		
+			// 【設定ファイル】読み込み
+			loadSetting()
 			
-			// 【作品一覧ファイル】読み込み 作品一覧 配置
-			loadWorklist();
+		).done(function(){
+		
+			// トップ配置
+			$('#id_main').load('temp_top.html', function() {
+			
+				const userAgent = window.navigator.userAgent.toLowerCase();
+				if(userAgent.indexOf('msie') != -1 || userAgent.indexOf('trident') != -1 ||) {
+					console.log('Internet Explorerをお使いですね');
+				}
 
-			// フッタ 配置
-			$('#id_footer').load('temp_footer.html');
+				// Viewインスタンス生成 【PICK UP配置】
+				const viewInstancePickup = new viewPickup({
+					// Collectionを渡す
+					collection:collectionInstanceSetting
+				});
+			
+				// 【作品一覧ファイル】読み込み 作品一覧 配置
+				loadWorklist();
 
+				// フッタ 配置
+				$('#id_footer').load('temp_footer.html');
+
+			});
 		});
-});
 	});
 
 }());
