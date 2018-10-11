a33c4d43e49f64572e72219407103b18a37437d0
hm0524 <38096718+hm0524@users.noreply.github.com>
2018-10-11 13:50:35 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,45 +5,18 @@
 	$('#id_menu').load('temp_menu.html', function() {
 
 		$.when(
-//		var promise = $.when(
-		
+
 			// 【設定ファイル】読み込み
 			loadSetting()
-//			let promise = loadSetting();
-//		);
-//		promise.done(function() {
+
 		).done(function(){
 		
 			// トップ配置
 			$('#id_main').load('temp_top.html', function() {
 
-
-		// Collectionインスタンス生成 【設定ファイル】
-//		createInstanceSetting(_data)
-
-		// Viewインスタンス生成
-//		const viewInstanceSetting = new viewSetting({
-//			// Collectionを渡す
-//			collection:collectionInstanceSetting
-//		});
-
-
-
-
-
-
-
-
-
 				// IE対応
 				moveHeadWidth();
 
-//				// Viewインスタンス生成 【PICK UP配置】
-//				const viewInstancePickup = new viewPickup({
-//					// Collectionを渡す
-//					collection:collectionInstanceSetting
-//				});
-			
 				// 【作品一覧ファイル】読み込み 作品一覧 配置
 				loadWorklist();
 
